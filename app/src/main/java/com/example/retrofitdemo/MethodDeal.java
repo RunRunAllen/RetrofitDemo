a3869ad47afdde3f5e7ff43fbf8726f0ed899297
Allen <yangle0610@qq.com>
2020-06-22 23:58:54 +0800

    JVM

---
+++ app/src/main/java/com/example/retrofitdemo/MethodDeal.java
@@ -0,0 +1,14 @@
+package com.example.retrofitdemo;
+
+public class MethodDeal {
+
+    public static void main(String[] args) {
+
+        //  max(1, 2);
+        boolean i = 1 > 2;
+    }
+
+    public static boolean max(int a, int b) {
+        return a > b;
+    }
+}
